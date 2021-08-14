a55b09b343e82c4a236d2ce5d8d0ebccf238146c
Perttu Ehn <perttu.ehn@exove.fi>
2021-08-14 16:34:49 +0300

    Overall improvements for web.py
    - Log IP addresses, prevent browser caching
    - use logger function
    etc.

--- web.py
+++ web.py
@@ -1,86 +1,116 @@
 import time
 from datetime import datetime
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, Response
 
 import RPi.GPIO as GPIO
-GPIO.setmode(GPIO.BOARD)  # the pin numbers refer to the board connector not the chip
+# the pin numbers refer to the board connector not the chip
+GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
-GPIO.setup(16, GPIO.IN, GPIO.PUD_UP) # set up pin ?? (one of the above listed pins) as an input with a pull-up resistor
-GPIO.setup(18, GPIO.IN, GPIO.PUD_UP) # set up pin ?? (one of the above listed pins) as an input with a pull-up resistor
-GPIO.setup(7, GPIO.OUT)
-GPIO.output(7, GPIO.HIGH)
+# set up pin ?? (one of the above listed pins) as an input with a pull-up resistor
+GPIO.setup(16, GPIO.IN, GPIO.PUD_UP)
+# set up pin ?? (one of the above listed pins) as an input with a pull-up resistor
+GPIO.setup(18, GPIO.IN, GPIO.PUD_UP)
+# GPIO.setup(7, GPIO.OUT)
+# GPIO.output(7, GPIO.HIGH)
 GPIO.setup(11, GPIO.OUT)
 GPIO.output(11, GPIO.HIGH)
-GPIO.setup(13, GPIO.OUT)
-GPIO.output(13, GPIO.HIGH)
-GPIO.setup(15, GPIO.OUT)
-GPIO.output(15, GPIO.HIGH)
+# GPIO.setup(13, GPIO.OUT)
+# GPIO.output(13, GPIO.HIGH)
+# GPIO.setup(15, GPIO.OUT)
+# GPIO.output(15, GPIO.HIGH)
+
+# Wtih static_url_path Flask serves all assets under the /static
+# with no further configuration.
+app = Flask(__name__, static_url_path='/static')
+
+OpenTriggerPassword = "12345678"
+VerboseConsole = False  # Wether or not print messages to console as well.
+
+# Fetch user real IP even if flask is running behind proxy.
 
 
+def logger(msg):
+    logfile = open("/home/pi/GarageWeb/static/log.txt", "a")
+    logfile.write(datetime.now().strftime(
+        "%Y-%m-%d %H:%M:%S [web.py] -- " + msg + "\n"))
+    logfile.close()
+    if VerboseConsole == True:
+        print(msg)
+
+# Fetch user real IP even if flask is running behind proxy.
 
 
-app = Flask(__name__)
+def user_ip_address():
+    user_ip = ''
+    if 'X-Forwarded-For' in request.headers:
+        proxy_data = request.headers['X-Forwarded-For']
+        ip_list = proxy_data.split(',')
+        user_ip = ip_list[0]  # first address in list is User IP
+    else:
+        user_ip = request.remote_addr  # For local development
+    return user_ip
 
-@app.route('/', methods=['GET', 'POST'])
+# Prevent all responses from being cached.
+
+
+@app.after_request
+def add_header(resp):
+    resp.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    resp.headers["Pragma"] = "no-cache"
+    resp.headers["Expires"] = "0"
+    resp.headers['Cache-Control'] = 'public, max-age=0'
+    return resp
+
+# Main route for GET requests.
+
+
+@app.route('/', methods=['GET'])
 def index():
-        if GPIO.input(16) == GPIO.HIGH and GPIO.input(18) == GPIO.HIGH:
-             print("Garage is Opening/Closing")
-             return app.send_static_file('Question.html')
-        else:  
-             if GPIO.input(16) == GPIO.LOW:
-                   print ("Garage is Closed")
-                   return app.send_static_file('Closed.html')
-             if GPIO.input(18) == GPIO.LOW:
-                   print ("Garage is Open")
-                   return app.send_static_file('Open.html')
+    user_ip = user_ip_address()
+    if GPIO.input(16) == GPIO.HIGH and GPIO.input(18) == GPIO.HIGH:
+        logger("(web) Garage is Opening/Closing")
+        return app.send_static_file('Question.html')
+    else:
+        if GPIO.input(16) == GPIO.LOW:
+            logger("(web) Garage is Closed")
+            return app.send_static_file('Closed.html')
+        if GPIO.input(18) == GPIO.LOW:
+            logger("(web) Garage is Open")
+            return app.send_static_file('Open.html')
+
+# Main route for POST requests (ie. door open/close requests)
 
 
-@app.route('/Garage', methods=['GET', 'POST'])
+@app.route('/', methods=['POST'])
 def Garage():
-        name = request.form['garagecode']
-        if name == '12345678':  # 12345678 is the Password that Opens Garage Door (Code if Password is Correct)
-                GPIO.output(7, GPIO.LOW)
-                time.sleep(1)
-                GPIO.output(7, GPIO.HIGH)
-                time.sleep(2)
+    user_ip = user_ip_address()
+    name = request.form['garagecode']
+    # the Password that Opens Garage Door (Code if Password is Correct)
+    if name == OpenTriggerPassword:
+        logger("Garage web triggered Opening/Closing (IP: " + user_ip + ")")
+        # This triggers the Opening/Closing the door.
+        GPIO.output(11, GPIO.LOW)
+        time.sleep(.5)
+        GPIO.output(11, GPIO.HIGH)
+        logger("Garage web triggered Opening/Closing completed.")
+        time.sleep(2)
+        return app.redirect('/', 200)
 
-                if GPIO.input(16) == GPIO.HIGH and GPIO.input(18) == GPIO.HIGH:
-                  print("Garage is Opening/Closing")
-                  return app.send_static_file('Question.html')
-                else:
-                  if GPIO.input(16) == GPIO.LOW:
-                        print ("Garage is Closed")
-                        return app.send_static_file('Closed.html')
-                  if GPIO.input(18) == GPIO.LOW:
-                        print ("Garage is Open")
-                        return app.send_static_file('Open.html')
+    else:
+        logger("Wrong password provided, request originated from IP " + user_ip)
+        return app.redirect('/', 400)
 
-        if name != '12345678':  # 12345678 is the Password that Opens Garage Door (Code if Password is Incorrect)
-                if name == "":
-                        name = "NULL"
-                print("Garage Code Entered: " + name)
-                if GPIO.input(16) == GPIO.HIGH and GPIO.input(18) == GPIO.HIGH:
-                  print("Garage is Opening/Closing")
-                  return app.send_static_file('Question.html')
-                else:
-                  if GPIO.input(16) == GPIO.LOW:
-                        print ("Garage is Closed")
-                        return app.send_static_file('Closed.html')
-                  if GPIO.input(18) == GPIO.LOW:
-                        print ("Garage is Open")
-                        return app.send_static_file('Open.html')
-
-@app.route('/stylesheet.css')
-def stylesheet():
-        return app.send_static_file('stylesheet.css')
 
 @app.route('/Log')
 def logfile():
-        return app.send_static_file('log.txt')
+    user_ip = user_ip_address()
+    logger("Access to route /Log from IP " + user_ip)
+    return app.send_static_file('log.txt')
 
-@app.route('/images/<picture>')
-def images(picture):
-        return app.send_static_file('images/' + picture)
 
 if __name__ == '__main__':
-        app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
+
+except KeyboardInterrupt:
+    logger("Web UI Shutdown -- Goodbye!")
+    GPIO.cleanup()
