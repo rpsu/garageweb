2dff48ecfa858f69c6a24cd7e65e1912f63ddddc
Perttu Ehn <perttu.ehn@exove.fi>
2021-08-23 20:14:05 +0300

    Remove unnecessary console prints

--- static/rest.js
+++ static/rest.js
@@ -5,24 +5,22 @@ const restApiDoor = () => {
     request.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
     request.responseType = 'json';
     request.onload = () => {
-        console.log(request)
         if (request.status === 200) {
             const resp = request.response;
             if (resp.status && resp.image && resp.color) {
-                document.body.style.backgroundColor = resp.color;
+                document.body.className = resp.color;
                 var image = document.getElementById('status-imge');
                 image.src = '/static/images/' + resp.image
             }
         } else {
             console.warn(`Response code is ${this.status}`)
-                // setTimeout(window.location = '/', 3000)
         }
-
     };
-    console.info('Calling API URL', API_URL);
     request.send();
 }
+
+// Call it immediately.
 restApiDoor();
 
-
+// Call it repeatedly.
 setInterval(restApiDoor, 15 * 1000)
