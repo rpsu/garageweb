1eef64216315d91ca0f605d2ca5d3f5ac06ddbfd
Perttu Ehn <perttu.ehn@yle.fi>
2025-06-03 18:47:54 +0300

    MAke door controls thread safe(r)

--- doorControls.py
+++ doorControls.py
@@ -1,16 +1,27 @@
+import atexit, signal, time, config, os.path, threading
 import RPi.GPIO as GPIO
-from datetime import datetime
-import time
-import config
-import os.path
+
 from utils import logger
 
 fileName = os.path.basename(__file__)
+lock = threading.Lock()
+GPIO_SETUP = None # Initialize as None
 
-# This triggers the Opening/Closing the door.
+# Shutdown and cleanup, then register the function right below.
+def shutdown(*args):
+    with lock:
+        GPIO.cleanup()
 
+atexit.register(shutdown)
+signal.signal(signal.SIGINT, lambda s, f: exit(0))
+signal.signal(signal.SIGTERM, lambda s, f: exit(0))
 
 def setup(initializer):
+    global GPIO_SETUP
+    if GPIO_SETUP:  # Check if already set up
+        logger("Init already done, skipping from " + initializer, fileName)
+        return True
+
     logger("Setting up GPIO Pins. Init requested from " + initializer, fileName)
     # Use BOARD mode. The pin numbers refer to the **BOARD** connector not the chip.
     # @see https://pinout.xyz/pinout/3v3_power# and the smaller numbers next to the PINs
@@ -33,38 +44,49 @@ def setup(initializer):
         GPIO.setup(config.PINS_BUTTON_CLOSE, GPIO.OUT)
         GPIO.output(config.PINS_BUTTON_OPEN, GPIO.HIGH)
 
+    GPIO_SETUP=True
     logger("Setting up GPIO Pins ... done!", fileName)
 
 
 # This triggers the Opening/Closing the door.
 def close(fromFile):
-    logger("Garage door closing triggered by " + fromFile, fileName)
-    GPIO.output(config.PINS_BUTTON_CLOSE, GPIO.LOW)
-    time.sleep(.5)
-    GPIO.output(config.PINS_BUTTON_CLOSE, GPIO.HIGH)
+    with lock:
+        msg = "Garage door closing triggered by " + fromFile
+        if config.DRY_RUN:
+            msg = "** DRY RUN ONLY ** " + msg
+        logger(msg, fileName)
+        GPIO.output(config.PINS_BUTTON_CLOSE, GPIO.LOW)
+        time.sleep(.5)
+        GPIO.output(config.PINS_BUTTON_CLOSE, GPIO.HIGH)
 
 
 # This triggers the Opening/Closing the door.
 def open(fromFile):
-    logger("Garage door opening triggered by " + fromFile, fileName)
-    GPIO.output(config.PINS_BUTTON_OPEN, GPIO.LOW)
-    time.sleep(.5)
-    GPIO.output(config.PINS_BUTTON_OPEN, GPIO.HIGH)
+    with lock:
+        msg = "Garage door opening triggered by " + fromFile
+        if config.DRY_RUN:
+            msg = "** DRY RUN ONLY ** " + msg
+        logger(msg, fileName)
+        GPIO.output(config.PINS_BUTTON_OPEN, GPIO.LOW)
+        time.sleep(.5)
+        GPIO.output(config.PINS_BUTTON_OPEN, GPIO.HIGH)
 
 
 # Read door status from magnetic switches connected to GPIO
 def status():
-    if GPIO.input(config.SWITCH_LOWER) == GPIO.HIGH and GPIO.input(config.SWITCH_UPPER) == GPIO.HIGH:
-        if config.LogLevel >= 1:
-            logger("Garage is Opening/Closing", fileName)
-        return config.STATE_BETWEEN
-    else:
-        if GPIO.input(config.SWITCH_LOWER) == GPIO.LOW:
-            if config.LogLevel >= 2:
-                logger("Garage is Closed", fileName)
-            return config.STATE_DOWN
-
-        if GPIO.input(config.SWITCH_UPPER) == GPIO.LOW:
+    # threading lock to avoid writing to log file at the same time.
+    with lock:
+        if GPIO.input(config.SWITCH_LOWER) == GPIO.HIGH and GPIO.input(config.SWITCH_UPPER) == GPIO.HIGH:
             if config.LogLevel >= 1:
-                logger("Door is Open", fileName)
-            return config.STATE_UP
+                logger("Garage is Opening/Closing", fileName)
+            return config.STATE_BETWEEN
+        else:
+            if GPIO.input(config.SWITCH_LOWER) == GPIO.LOW:
+                if config.LogLevel >= 2:
+                    logger("Garage is Closed", fileName)
+                return config.STATE_DOWN
+
+            if GPIO.input(config.SWITCH_UPPER) == GPIO.LOW:
+                if config.LogLevel >= 1:
+                    logger("Door is Open", fileName)
+                return config.STATE_UP
