a2ce497262122d25c62f9a12e1a8f3d910f8ceb8
Perttu Ehn <perttu.ehn@exove.fi>
2025-06-14 09:50:46 +0300

    Add filename, time to config tester

--- test-config.py
+++ test-config.py
@@ -1,5 +1,6 @@
-import os.path, threading, datetime
-from flask import jsonify
+import os.path
+from datetime import datetime
+from flask import Flask, request, jsonify
 from dotenv import dotenv_values
 
 # Load .env files
@@ -7,7 +8,8 @@ config = {
     **dotenv_values(".env.default"),  # load what came with the repo
     **dotenv_values(".env"),  # load overrides from the local
 }
-print(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+print("File: " + os.path.basename(__file__))
+print("Time: " + datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
 print("config from dotenv: " + jsonify(config))
 
  
