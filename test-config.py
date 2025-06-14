33f7ba1b574bf2682b4bd9e3bdac4ea2b0b39291
Perttu Ehn <perttu.ehn@exove.fi>
2025-06-14 09:53:10 +0300

    Refactor config tester

--- test-config.py
+++ test-config.py
@@ -3,13 +3,16 @@ from datetime import datetime
 from flask import Flask, request, jsonify
 from dotenv import dotenv_values
 
+print("File: " + os.path.basename(__file__))
+print("Time: " + datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+print("Loading config.")
 # Load .env files
 config = {
     **dotenv_values(".env.default"),  # load what came with the repo
     **dotenv_values(".env"),  # load overrides from the local
 }
-print("File: " + os.path.basename(__file__))
-print("Time: " + datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+print("Config loaded: " + str(config.__len__) + " items.")
 print("config from dotenv: " + jsonify(config))
+print("Done.")
 
  
