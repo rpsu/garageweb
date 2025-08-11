333fc8ec0f389631998d9c546bd70b44762cf383
Perttu Ehn <perttu.ehn@yle.fi>
2025-08-11 17:25:12 +0300

    Remove duplicate cache control header

--- doorWeb.py
+++ doorWeb.py
@@ -48,7 +48,6 @@ def add_header(resp):
     resp.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
     resp.headers["Pragma"] = "no-cache"
     resp.headers["Expires"] = "0"
-    resp.headers['Cache-Control'] = 'public, max-age=0'
     return resp
 
 
